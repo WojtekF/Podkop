3ddffc8ac04ea4eb4e3c227f672160beef5bea83
WojtekF <wfabianczuk@gmail.com>
2026-07-06 14:25:23 +0200

    modifying up/down vote

--- frontend/src/sink/post-card/post-card.ts
+++ frontend/src/sink/post-card/post-card.ts
@@ -1,14 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MainPost } from '../sink.service';
 
 @Component({
   selector: 'sink-post-card',
-  imports: [MatCardModule, DatePipe],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, DatePipe],
   templateUrl: './post-card.html',
   styleUrl: './post-card.scss',
 })
 export class PostCard {
   mainPost = input.required<MainPost>();
+
+  upvote = output<void>();
+  downvote = output<void>();
 }
