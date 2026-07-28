d866728d520d9f0a7c4516e8132c060835cbd6b1
WojtekF <wfabianczuk@gmail.com>
2026-07-28 13:03:14 +0200

    feat: creating comment thread and row

--- frontend/src/finding-detail/comment-row/comment-row.ts
+++ frontend/src/finding-detail/comment-row/comment-row.ts
@@ -1,12 +1,15 @@
 import { Component, input } from '@angular/core';
 import { CommentDto } from '../finding-comments.service';
+import { TimeAgoPipe } from './timeAgo.pipe';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-comment-row',
-  imports: [],
+  imports: [TimeAgoPipe, MatCard, MatCardContent],
   templateUrl: './comment-row.html',
   styleUrl: './comment-row.scss',
 })
 export class CommentRow {
+  readonly isReply = input<boolean>(true);
   readonly comment = input.required<CommentDto>();
 }
--- frontend/src/finding-detail/comment-thread/comment-thread.ts
+++ frontend/src/finding-detail/comment-thread/comment-thread.ts
@@ -1,9 +1,11 @@
 import { Component, input } from '@angular/core';
 import { CommentThreadDto } from '../finding-comments.service';
+import { CommentRow } from '../comment-row/comment-row';
+import { MatCard, MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'app-comment-thread',
-  imports: [],
+  imports: [CommentRow, MatCard, MatCardContent],
   templateUrl: './comment-thread.html',
   styleUrl: './comment-thread.scss',
 })
