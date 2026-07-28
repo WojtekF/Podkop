f7f8abd1128079cb68abccc41bc6d0a296ba9927
WojtekF <wfabianczuk@gmail.com>
2026-07-28 14:19:11 +0200

    fix: removing slopoy comment

--- Features/Findings/Podkop.Findings.Domain/Finding.cs
+++ Features/Findings/Podkop.Findings.Domain/Finding.cs
@@ -62,7 +62,7 @@ public sealed class Finding
         _domainEvents.Add(new FindingPromoted(Id, promotedAt));
     }
 
-    // method exposed for seeding purpose only. will be deleted in nearest future.
+    // method exposed for seeding purpose only.
     public void UpdateCommentCount(int commentCount)
     {
         CommentCount = commentCount;
